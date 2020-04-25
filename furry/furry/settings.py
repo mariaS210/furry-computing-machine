87f5081ada8b8d969c4d701b0bf9f08c89c0ab36
Maria S <maria.sumedre@gmail.com>
2020-04-25 17:13:59 +0300

    add static root

--- furry/furry/settings.py
+++ furry/furry/settings.py
@@ -131,6 +131,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = 'furry/staticfiles'
 
 # Configure Django App for Heroku.
 # import django_heroku
